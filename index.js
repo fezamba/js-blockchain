92570b7bc0251c1823a080bfca95a62c22fb5449
fezamba <fepetitt@gmail.com>
2025-03-20 14:42:19 -0300

    Feat isChainValid

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ class Blockchain {
     }
 
     getPreviousBlock() {
-        return this.chain[-1];
+        return this.chain[this.chain.length - 1];
     }
 
     proofOfWork(previousProof) {
@@ -53,11 +53,28 @@ class Blockchain {
             obj[key] = block[key];
             return obj;
         }, {});
-        const encodedBlock = JSON.stringify(block, Object.keys(block).sort());
+        const encodedBlock = JSON.stringify(sortedBlock);
         return crypto.createHash("sha256").update(encodedBlock).digest("hex");
     }
 
     isChainValid(chain) {
+        let previousBlock = chain[0];
+        let blockIndex = 1;
+        while (blockIndex < chain.length){
+           let block = chain[blockIndex];
+           if (block["previousHash"] !== this.hash(previousBlock)){
+            return false;
+           }
+           let previousProof = previousBlock["proof"];
+           let proof = block["proof"];
+           let hashOperation = crypto.createHash('sha256').update(String(proof ** 2 - previousProof ** 2)).digest('hex');
+           if (hashOperation.substring(0, 4) !== "0000"){
+            return false;
+           }
+           previousBlock = block;
+           blockIndex += 1;
+        }
+        return true;
     }
 
     addTransaction(sender, receiver, amount) {
@@ -70,16 +87,6 @@ class Blockchain {
     }
 }
 
-/* app.get('/', (req, res) => {
-    res.json({ mensagem: 'Servidor Express rodando com import!' });
-});
-
+/*
 const id = uuidv4();
-console.log(`UUID Gerado: ${id}`);
-
-const hash = crypto.createHash('sha256').update('texto').digest('hex');
-console.log(`Hash SHA-256: ${hash}`);
-
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-}); */
+console.log(`UUID Gerado: ${id}`);*/
